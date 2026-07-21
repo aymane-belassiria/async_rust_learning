82ca5bc60ac69dc20e5ed594ee9e5a9db39ad706
belassiria aymane <aymanebel2@outlook.fr>
2026-07-21 21:44:32 +0100

    shared mutex

--- src/chapters/ch2.rs
+++ src/chapters/ch2.rs
@@ -1,5 +1,5 @@
 use std::thread;
-use std::sync::Arc;
+use std::sync::{Arc, Mutex};
 pub fn shared_with_arc(){
     let data = Arc::new(vec![1,2,3]);
     let mut handles = vec![];
@@ -8,4 +8,18 @@ pub fn shared_with_arc(){
         handles.push(thread::spawn(move||println!("Thread {i} sees sum: {}", data.iter().sum::<i32>())));
     }
     for h in handles { h.join().unwrap();}
+}
+
+pub fn shared_with_mutex(){
+    let counter = Arc::new(Mutex::new(0));
+    let mut handles = vec![];
+    for _ in 0..9{
+        let counter = Arc::clone(&counter);
+        handles.push(thread::spawn(move ||{
+            let mut num = counter.lock().unwrap();
+            *num += 1; 
+        }));
+    }
+    for h in handles { h.join().unwrap(); }
+    println!("Final count: {}", counter.lock().unwrap());
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use learn_asyn_rust::chapters::ch2;
 fn main() {
     ch2::shared_with_arc();
+    ch2::shared_with_mutex();
 }
